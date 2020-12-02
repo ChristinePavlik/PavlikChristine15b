4e629819c7a5b2090fd784a6898a5917a7f24c39
Christine Pavlik <christine.pavlik@sinclair.edu>
2020-12-01 20:50:01 -0500

    Add project files.

---
+++ PavlikChristine15b/Source.cpp
@@ -0,0 +1,33 @@
+/*
+Christine Pavlik
+December 6
+CIS 1202 501
+*/
+#include <iostream>
+#include <cmath>
+using namespace std;
+///////////////////////////////////////////
+template <class T>
+T half(T number) {
+	return number / 2;
+}
+template <>
+int half(int number) {
+	float num = static_cast<float>(number);
+	num = num / 2.0;
+	num = round(num);
+	number = static_cast<int>(num);
+	return number;
+}
+
+int main() {
+	double a = 7.0;
+	float b = 5.0f;
+	int c = 3;
+
+	cout << half(a) << endl;
+	cout << half(b) << endl;
+	cout << half(c) << endl;
+
+	return 0;
+}
